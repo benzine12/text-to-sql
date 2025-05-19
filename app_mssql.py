0cd85562c68e6e2ddecb3c38f51acbaae0f6823c
benzine12 <176717159+benzine12@users.noreply.github.com>
2025-05-19 11:39:13 +0300

    added logs

--- app_mssql.py
+++ app_mssql.py
@@ -5,9 +5,31 @@ from dotenv import load_dotenv
 from flask import Flask, request
 from flask import jsonify
 import json
+import logging
 
 # load data from .env
 load_dotenv()
+
+# Configure the root logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# Create handlers
+console_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('app.log')
+
+console_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.DEBUG)
+
+# Create formatters and add them to handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+# Add handlers to the logger
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 # db configuration
 DB_CONFIG = {
